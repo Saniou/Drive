c452c2ad678e668fd8ae5ebcf10aae62d1ccaf4d
Olexander Krasulia <krasulazr@gmail.com>
2023-11-13 22:54:50 +0200

    bug fix 2

--- components/MapBoxRoute.tsx
+++ components/MapBoxRoute.tsx
@@ -1,23 +1,36 @@
-import React from 'react'
-import { Layer, Source } from 'react-map-gl'
+import React from 'react';
+import { Layer, Source } from 'react-map-gl';
+
+type LineStringGeometry = {
+    type: 'LineString';
+    coordinates: number[][];
+};
+
+type Feature = {
+    type: 'Feature';
+    properties: {};
+    geometry: LineStringGeometry;
+};
 
 function MapBoxRoute(props: any) {
+    const data: Feature = {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+            type: 'LineString',
+            coordinates: props.coordinates,
+        },
+    };
+
     return (
-        <Source type='geojson' data={{
-            type: 'Feature',
-            properties: {},
-            geometry: {
-                type: 'LineString',
-                coordinates: props.coordinates
-            }
-        }}>
+        <Source type='geojson' data={data}>
             <Layer
                 type='line'
                 layout={{ 'line-join': 'round', 'line-cap': 'square' }}
                 paint={{ 'line-color': '#f3c4dc', 'line-width': 4 }}
             />
         </Source>
-    )
+    );
 }
 
-export default MapBoxRoute
+export default MapBoxRoute;
